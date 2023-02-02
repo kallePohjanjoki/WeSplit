b6019649701b76ff15e404b705b0b72e6c3afb4a
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2023-02-02 20:40:13 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kalle Pohjanjoki on 2.2.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
