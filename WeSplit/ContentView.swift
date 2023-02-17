e47e4fae4654e1dacf272d23c6b925d6a194688d
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2023-02-17 11:55:34 +0200

    Total amount text is now red if tip is 0%

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -4,34 +4,34 @@
 //
 //  Created by Kalle Pohjanjoki on 2.2.2023.
 //
-
+//
 import SwiftUI
 
 struct ContentView: View {
-	
+
 	@State private var checkAmount = 0.0
 	@State private var numberOfPeople = 2
-	@State private var tipPercentage = 20.0
+	@State private var tipPercentage = 20
 	@FocusState private var amountIsFocused: Bool
-	
-	
-	
+
+
+
 	let tipPercentageOptions: Array<Int> = [10, 15, 20, 25, 0]
-	
+
 	var completeTotal: Double {
-		checkAmount + checkAmount * (tipPercentage / 100)
+		checkAmount + checkAmount * (Double(tipPercentage) / 100)
 	}
-	
+
 	var totalPerPerson: Double {
 		let peopleCount = Double (numberOfPeople + 2)
 		let tipSelection = Double(tipPercentage)
-		
+
 		let totalTip = checkAmount / 100 * tipSelection
-		
+
 		let grandTotal = (checkAmount + totalTip) / peopleCount
 		return grandTotal
 	}
-	
+
 	var body: some View {
 		NavigationView {
 			Form {
@@ -39,16 +39,16 @@ struct ContentView: View {
 					TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
 					.keyboardType(.decimalPad)
 					.focused($amountIsFocused)
-					
+
 					Picker("Number of people", selection: $numberOfPeople) {
 						ForEach(2..<100) {
 							Text("\($0) people")
 						}
 					}
 				}
-				
+
 				Section {
-					
+
 					Picker("Tip percentage", selection: $tipPercentage) {
 						ForEach(tipPercentageOptions, id: \.self) {
 							Text($0, format: .percent)
@@ -58,17 +58,18 @@ struct ContentView: View {
 				} header: {
 					Text("How much tip to give?")
 				}
-				
+
 				Section {
 					Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-					
+
 				} header: {
 					Text("Total amount per person")
 				}
-				
+
 				Section {
 					Text(completeTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-					
+						.foregroundColor(tipPercentage == 0 ? .red : .black)
+
 				} header: {
 					Text("Total check amount")
 				}
@@ -77,16 +78,16 @@ struct ContentView: View {
 			.toolbar {
 				ToolbarItemGroup(placement: .keyboard) {
 					Spacer()
-					
+
 					Button("Done") {
 						amountIsFocused = false
 					}
 				}
 			}
-			
+
 		}
-		
-		
+
+
 	}
 }
 
@@ -100,3 +101,99 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Kalle Pohjanjoki on 2.2.2023.
+//
+
+//import SwiftUI
+//
+//struct ContentView: View {
+//
+//	@State private var checkAmount = 0.0
+//	@State private var numberOfPeople = 2
+//	@State private var tipPercentage = 20
+//	@FocusState private var amountIsFocused: Bool
+//
+//
+//
+//	let tipPercentageOptions: Array<Int> = [10, 15, 20, 25, 0]
+//
+//
+//	var completeTotal: Double {
+//		checkAmount + checkAmount * (tipPercentage / 100)
+//	}
+//
+//	var totalPerPerson: Double {
+//		let peopleCount = Double (numberOfPeople + 2)
+//		let tipSelection = Double(tipPercentage)
+//
+//		let totalTip = checkAmount / 100 * tipSelection
+//
+//		let grandTotal = (checkAmount + totalTip) / peopleCount
+//		return grandTotal
+//	}
+//
+//	var body: some View {
+//		NavigationView {
+//			Form {
+//				Section {
+//					TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+//					.keyboardType(.decimalPad)
+//					.focused($amountIsFocused)
+//
+//					Picker("Number of people", selection: $numberOfPeople) {
+//						ForEach(2..<100) {
+//							Text("\($0) people")
+//						}
+//					}
+//				}
+//
+//				Section {
+//
+//					Picker("Tip percentage", selection: $tipPercentage) {
+//						ForEach(tipPercentageOptions, id: \.self) {
+//							Text($0, format: .percent)
+//						}
+//					}
+//					.pickerStyle(.segmented)
+//				} header: {
+//					Text("How much tip to give?")
+//				}
+//
+//				Section {
+//					Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+//
+//				} header: {
+//					Text("Total amount per person")
+//				}
+//			}
+//			.navigationTitle("WeSplit")
+//			.toolbar {
+//				ToolbarItemGroup(placement: .keyboard) {
+//					Spacer()
+//
+//					Button("Done") {
+//						amountIsFocused = false
+//					}
+//				}
+//			}
+//
+//		}
+//
+//
+//	}
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//	static var previews: some View {
+//		ContentView()
+//	}
+//}
+//
+//
+//
+//
+//
